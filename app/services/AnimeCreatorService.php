962f5dc055d6fe331c3ef95bec205cbb1ad7c765
Carlos Vinicius <viniciusdev.26@gmail.com>
2021-11-01 20:49:37 -0300

    refactor: use transaction to ensure integrity of data and refactor AnimeCreatorService.php

--- app/services/AnimeCreatorService.php
+++ app/services/AnimeCreatorService.php
@@ -3,26 +3,45 @@
 namespace App\services;
 
 use App\Models\Anime;
+use Illuminate\Support\Facades\DB;
 
 class AnimeCreatorService
 {
-    public function execute(string $name, int $seasons, int $episodes)
+    private function createAnime(string $name)
     {
-        $anime = Anime::create([
+        return Anime::create([
             'name' => $name
         ]);
+    }
 
-        for($i = 0; $i < $seasons; $i++){
+    private function createEpisodes($season, int $numberOfEpisodesPerSeason)
+    {
+        for($j = 0; $j < $numberOfEpisodesPerSeason; $j++){
+            $season->episodes()->create([
+                'number' => $j
+            ]);
+        }
+    }
+
+    private function createSeasons(Anime $anime, int $numberOfSeasons, int $numberOfEpisodesPerSeason)
+    {
+        for($i = 1; $i < $numberOfSeasons; $i++){
             $season = $anime->seasons()->create([
                 'number' => $i
             ]);
 
-            for($j = 0; $j < $episodes; $j++){
-                $season->episodes()->create([
-                    'number' => $j
-                ]);
-            }
+            $this->createEpisodes($season, $numberOfEpisodesPerSeason);
         }
+    }
+
+    public function execute(string $name, int $seasons, int $episodes): Anime | null
+    {
+        DB::beginTransaction();
+
+        $anime = $this->createAnime($name);
+        $this->createSeasons($anime, $seasons, $episodes);
+
+        DB::commit();
 
         return $anime;
     }
