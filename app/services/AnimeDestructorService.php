d7a1c411dea0e8c2cc1f3e365b36def6bf908c9d
Carlos Vinicius <viniciusdev.26@gmail.com>
2021-11-01 20:21:42 -0300

    refactor: use transaction to ensure integrity of data and refactor AnimeDestructorService.php

--- app/services/AnimeDestructorService.php
+++ app/services/AnimeDestructorService.php
@@ -3,23 +3,49 @@
 namespace App\services;
 
 use App\Models\Anime;
-use App\Models\Episodes;
+use App\Models\Episode;
 use App\Models\Season;
+use Illuminate\Support\Facades\DB;
 
 class AnimeDestructorService
 {
-    public function execute(string $uuid)
-    {
-        $anime = Anime::find($uuid);
-        $anime->seasons->each(function(Season $season) {
-            $season->episodes->each(function(Episodes $episode) {
-                $episode->delete();
-            });
 
+    /**
+     * @param Season $season
+     */
+    private function destroyEpisodes(Season $season) {
+        Episode::where('season_id', $season->id)->each(function (Episode $episode) {
+            $episode->delete();
+        });
+    }
+
+    /**
+     * @param Anime $anime
+     */
+    private function destroySeasons(Anime $anime) {
+        $anime->seasons->each(function(Season $season) {
+            $this->destroyEpisodes($season);
             $season->delete();
         });
+    }
 
+    /**
+     * @param Anime $anime
+     */
+    private function destroyAnime(Anime $anime): void
+    {
+        $this->destroySeasons($anime);
         $anime->delete();
+    }
+
+    public function execute(string $uuid)
+    {
+        $anime = '';
+
+        DB::transaction(function() use ($uuid, &$anime) {
+            $anime = Anime::find($uuid);
+            $this->destroyAnime($anime);
+        });
 
         return $anime;
     }
